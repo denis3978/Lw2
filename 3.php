f3e964cc862a596e5c6891f598fb38a5b324c17b
Денис <den2007pr@gmail.com>
2026-04-04 02:40:07 +0300

    3.php

--- 3.php
+++ 3.php
@@ -1,23 +1,15 @@
 <?php
 
-echo "Введите элементы массива через пробел: ";
-$input = trim(fgets(STDIN));
+function cycleShiftRight(array $arr): array
+{
+    if (empty($arr)) {
+        return [];
+    }
 
-if ($input == "") {
-    $arr = [1, 2, 3, 4, 5];
-    echo "Используем массив по умолчанию: ";
-    print_r($arr);
-} else {
-    $arr = array_map('intval', explode(' ', $input));
+    $last = array_pop($arr);
+    array_unshift($arr, $last);
+
+    return $arr;
 }
 
-echo "\nИсходный массив:\n";
-echo implode(", ", $arr) . "\n";
-
-if (count($arr) > 0) {
-    $last_element = array_pop($arr);
-    array_unshift($arr, $last_element);
-}
-
-echo "Массив после сдвига вправо:\n";
-echo implode(", ", $arr) . "\n";
+var_dump(cycleShiftRight([1, 2, 3, 4, 5]));
