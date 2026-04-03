343381aad1e6aa0710d7811845cd6b4b9f6c56a8
Денис <den2007pr@gmail.com>
2026-04-04 02:39:35 +0300

    2.php

--- 2.php
+++ 2.php
@@ -1,32 +1,24 @@
 <?php
 
-$matrix = [
-    [5, 8, 3],
-    [12, 4, 7],
-    [9, 2, 15]
-];
+function findMaxInMatrix(array $matrix): int
+{
+    $max = $matrix[0][0];
 
-echo "Матрица:\n";
-foreach ($matrix as $row) {
-    foreach ($row as $value) {
-        echo str_pad($value, 4, " ", STR_PAD_LEFT);
-    }
-    echo "\n";
-}
-
-$max_value = $matrix[0][0];
-$max_row = 0;
-$max_col = 0;
-
-for ($i = 0; $i < count($matrix); $i++) {
-    for ($j = 0; $j < count($matrix[$i]); $j++) {
-        if ($matrix[$i][$j] > $max_value) {
-            $max_value = $matrix[$i][$j];
-            $max_row = $i;
-            $max_col = $j;
+    foreach ($matrix as $row) {
+        foreach ($row as $value) {
+            if ($value > $max) {
+                $max = $value;
+            }
         }
     }
+
+    return $max;
 }
 
-echo "\nМаксимальный элемент: $max_value\n";
-echo "Позиция: строка " . ($max_row + 1) . ", столбец " . ($max_col + 1) . "\n";
+$matrix = [
+    [1, 5, 3],
+    [8, 2, 9],
+    [4, 7, 6]
+];
+
+var_dump(findMaxInMatrix($matrix));
