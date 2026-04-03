b5c563b3d4a8566ea6b622f73da2c1f55f1753f4
Денис <den2007pr@gmail.com>
2026-04-04 02:39:02 +0300

    1.php

--- 1.php
+++ 1.php
@@ -1,25 +1,21 @@
 <?php
 
+function calculateAverageAge(array $users): float
+{
+    $sum = 0;
+    $count = count($users);
+
+    foreach ($users as $user) {
+        $sum += $user['age'];
+    }
+
+    return $sum / $count;
+}
+
 $users = [
-    ["name" => "Ivan", "age" => 25],
-    ["name" => "Petr", "age" => 17],
-    ["name" => "Olga", "age" => 30]
+    ['name' => 'Ivan', 'age' => 25],
+    ['name' => 'Petr', 'age' => 17],
+    ['name' => 'Olga', 'age' => 30]
 ];
 
-echo "Список пользователей:\n";
-foreach ($users as $user) {
-    echo "{$user['name']} - {$user['age']} лет\n";
-}
-
-$sum_age = 0;
-$count = count($users);
-
-foreach ($users as $user) {
-    $sum_age += $user['age'];
-}
-
-$average_age = $sum_age / $count;
-
-echo "\nСумма возрастов: $sum_age\n";
-echo "Количество пользователей: $count\n";
-echo "Средний возраст: " . round($average_age, 2) . " лет\n";
+var_dump(calculateAverageAge($users));
